7ca7cd5eeb099dd6aae64d14c055a58af8c3424b
luugiahuy123 <luugiahuyqn@gmail.com>
2023-10-11 16:44:35 +0700

    bai_tap

--- ss8/cannangcothe/can_nang.js
+++ ss8/cannangcothe/can_nang.js
@@ -2,15 +2,14 @@ function kiemtra() {
 let weight = +document.getElementById("can_nang").value;
 let height = +document.getElementById("chieu_cao").value;
 let bmi = weight / (height * height);
-
-        if (bmi < 18)
-            document.getElementById("result").innerHTML = "gầy";
-        else if (bmi < 25)
-            document.getElementById("result").innerHTML = "vừa";
-        else if (bmi < 30)
-            document.getElementById("result").innerHTML = "mập";
-        else
-            document.getElementById("result").innerHTML = "quá mập";
+if (bmi < 18)
+    document.getElementById("result").innerHTML = "gầy";
+else if (bmi < 25)
+    document.getElementById("result").innerHTML = "vừa";
+else if (bmi < 30)
+    document.getElementById("result").innerHTML = "mập";
+else
+    document.getElementById("result").innerHTML = "quá mập";
 }
 
 
