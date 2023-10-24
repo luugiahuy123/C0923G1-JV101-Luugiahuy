ac318ac34fb200fc7baccb6ed44743294b3a3df5
luugiahuy123 <luugiahuyqn@gmail.com>
2023-10-24 17:11:07 +0700

    bai_tap

--- ss7/ung_dung_may_tinh/ung_dung_may_tinh.js
+++ ss7/ung_dung_may_tinh/ung_dung_may_tinh.js
@@ -1,7 +1,7 @@
 function add() {
     let number1 = document.getElementById("num1").value;
     let number2 = document.getElementById("num2").value;
-    let result = Number(number1) + Number(number2); //16
+    let result = Number(number1) + Number(number2);
     document.getElementById("result").innerHTML = result;
 }
 
